2df28efb7ba78016a9d72297f09d374675675e7d
sangikHwang <hwangsangik@gamil.com>
2024-04-29 16:13:58 +0900

    mvc1

---
+++ src/main/java/servlet/servlet_1/basic/HelloServlet.java
@@ -0,0 +1,31 @@
+package servlet.servlet_1.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+
+        //WAS가 req, resp를 만들어서 내려준다.
+        //Servlet 표준 spack을 구현
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        //HTTP Body에 내용 들어간다.
+        response.getWriter().write("hello " + username);
+    }
+}
---
+++ src/test/java/servlet/servlet_1/Servlet1ApplicationTests.java
@@ -0,0 +1,13 @@
+package servlet.servlet_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servlet1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
